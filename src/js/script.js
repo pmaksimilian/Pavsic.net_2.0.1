710dfef5072f55b3bd997115db9aaead02fe7658
pmaksimilian <pavsicmaks@gmail.com>
2022-10-31 12:23:09 +0100

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,31 @@
+
+let mainTitle = document.getElementsByClassName("mainTitle")[0];
+let mainTitlePosition = mainTitle.offsetLeft;
+
+
+
+// INTRO ANIMATION
+
+let animationDuration = 7000;
+
+
+// disable scroll at start
+setTimeout(() => {
+    let main = document.getElementsByClassName("main-wrapper")[0];
+    main.classList.remove("stopScroll");
+}, animationDuration);
+
+// show and hide welcome title and header
+let title = document.getElementsByClassName("mainTitleContainer")[0];
+let header = document.getElementsByClassName("header")[0];
+let boxes = document.getElementsByClassName("mainBoxesContainer")[0];
+
+title.classList.add("displayBlock");
+
+setTimeout(() => {
+    header.classList.add("full");
+    setTimeout(() => {
+        title.classList.remove("displayBlock");
+        boxes.classList.add("show");
+    }, 200);
+}, animationDuration);
